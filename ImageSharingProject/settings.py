185a70ff8d799074761bee73974c1b68eba9d022
Carl Nydén <carl.nyden@gmail.com>
2021-04-12 13:19:28 +0200

    Fixed html for homepage

--- ImageSharingProject/settings.py
+++ ImageSharingProject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ImageSharingProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR,"Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
