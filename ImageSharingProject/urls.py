b0774cf9277bab5101badfab48178aa9cb781b7f
JesperThCplus <jthiger@gmail.com>
2021-04-15 15:09:52 +0200

    Urls fixed "images"

--- ImageSharingProject/urls.py
+++ ImageSharingProject/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
     path("registration/", include("registration.urls")),
+    path("images/", include("images.urls"))
 ]
