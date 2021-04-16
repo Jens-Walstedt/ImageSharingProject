51a960e2ac110d6ec78dca88892f6f55b0284241
Jens Wålstedt <jens.walstedt@gmail.com>
2021-04-16 14:06:32 +0200

    Renamed registration app to users and made all changed related to it.

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-from .forms import RegisterForm
+from .forms import UserForm
 
 
 def created_user_view(request, id):
@@ -11,15 +11,15 @@ def created_user_view(request, id):
 
 
 def register_user_view(request):
-    form = RegisterForm()
+    form = UserForm()
     if request.method == "POST":
-        form = RegisterForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             user = authenticate(request, username=form.cleaned_data.get("username"), password=form.cleaned_data.get("password1"))
             if user is not None:
                 login(request, user)
-                return redirect("registration/success/" + str(user.pk))
+                return redirect("users/success/" + str(user.pk))
             else:
                 return render(request, "register.html", {"form": form})
     
