53800cb700b4b0ed9eb50fb1a7882e6bcf86a92d
Jens Wålstedt <jens.walstedt@gmail.com>
2021-04-19 14:24:33 +0200

    added profile picture

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import created_user_view, register_user_view
+from .views import created_user_view, register_user_view, profile_view
 
 
 urlpatterns = [
+    path("", profile_view, name="profile"), 
     path("success/<int:id>", created_user_view, name="created_user"),
     path("register/", register_user_view, name="register"),
+    
 ]
 
 # Qwerty4321
