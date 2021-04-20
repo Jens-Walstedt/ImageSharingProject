a39cfc7fa8eed11e9c988829f38a29b138392035
Jens Wålstedt <jens.walstedt@gmail.com>
2021-04-20 10:31:25 +0200

    Added ImageItemManager for saving saving images tied to user

--- users/models.py
+++ users/models.py
@@ -8,10 +8,19 @@ from images.models import Image
     #userId = models.ForeignKey(User, on_delete=models.CASCADE)
     #profile_pic = models.ForeignKey(, on_delete=models.CASCADE, null=True)
 
+
+class ImageItemManager(models.Manager):
+    def create_ImageItem(self, userId, imageId, imageOwner):
+        ImageItem = self.create(userId=userId, imageId=imageId, imageOwner=imageOwner)
+        # do something with the book
+        return ImageItem
+
 DEFAULT_ID = 1
 class ImageItem(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE, default=DEFAULT_ID)
     imageId = models.ForeignKey(Image, on_delete=models.CASCADE)
     imageOwner = models.BooleanField(default=False)
+
+    objects = ImageItemManager()
     
    
