58d8d3e7c1eea27b73b062ec1eddfab46ccc4fdd
Jens Wålstedt <jens.walstedt@gmail.com>
2021-04-20 10:33:27 +0200

    Logged in user now create imageItem when upload image

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ImageForm
 from .models import Image
+from users.models import ImageItem, ImageItemManager
 
 def edit_image_view(request):
     
@@ -16,8 +17,10 @@ def image_upload_view(request):
     if request.method == "POST":
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            #logic add imageitem
+            task = form.save() 
+            ImageItem.objects.create_ImageItem(userId=request.user,
+            imageId=task,
+            imageOwner=True)
             return redirect("images_list")
 
             
